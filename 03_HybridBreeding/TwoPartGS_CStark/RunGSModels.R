789997e5654cd2c6982318e09afd90b4ef04e92e
Cristian Stark <crstark@ucdavis.edu>
2024-05-05 15:25:41 -0700

    figuring out how to subset emBB output
    I ran what I thought was the subset of emBB output, but I only thought about YT1 not DH. I need to run the model on both somehow

--- 03_HybridBreeding/TwoPartGS_CStark/RunGSModels.R
+++ 03_HybridBreeding/TwoPartGS_CStark/RunGSModels.R
@@ -8,8 +8,8 @@ if(exists("bayesB")){
   female_geno_MC <- data.frame(NULL);
   female_pheno_MC <- data.frame(NULL);
   
-  maleGen = pullSnpGeno(MaleTrainPop, simParam = SP)
-  maleYT1Gen = pullSnpGeno(MaleYT1, simParam = SP)
+  #maleGen = pullSnpGeno(MaleTrainPop, simParam = SP)
+  #maleYT1Gen = pullSnpGeno(MaleYT1, simParam = SP)
   malesPop <- c(MaleTrainPop,MaleYT1)
   malesGen <- pullSnpGeno(malesPop, simParam = SP)
   male_geno_MC <- rbind(data.frame(i=1,maleGen),male_geno_MC)
@@ -18,7 +18,7 @@ if(exists("bayesB")){
   ### below owuld include geno data just for new pop... which doesn't seem right.
   #gsModelM = emBB(as.matrix(male_pheno_MC[,-1]),maleYT1Gen)
   ### below would incorporate geno data for training and for new pop
-  #gsModelM = emBB(as.matrix(male_pheno_MC[,-1]),malesGen)
+  gsModelM = emBB(as.matrix(male_pheno_MC[,-1]),malesGen)
   
   # got this code from the Features/Techniques section -> genomic models
   #maleG = A.mat(pullSnpGeno(MaleTrainPop, snpChip = 2)-1)
@@ -29,18 +29,24 @@ if(exists("bayesB")){
   if(anyNA(gsModelM$hat)){
     gsModelM$hat=rnorm(length(fit$hat))
   } 
-  ### Need to figure out how to use full Genos but only get new pop data. Subset out the num in training pop?
-  gsModelM = as.matrix(gsModelM$hat[male_pheno_MC$i == i],ncol=1)
+  ### Need to figure out how to use full Genos but only get new pop data. 
+  gsModelAllMales = gsModelM$hat
+  ###Subset out the num in training pop?
+  gsModelM <- as.matrix(gsModelAllMales[(nrow(male_pheno_MC)+1):length(gsModelAllMales)],ncol=1)
   
-  femaleGen = pullSnpGeno(FemaleTrainPop, simParam = SP)
-  female_geno_MC <- rbind(data.frame(i=1,femaleGen),female_geno_MC)
+  femalesPop <- c(FemaleTrainPop,FemaleYT1)
+  femalesGen <- pullSnpGeno(femalesPop, simParam = SP)
+  female_geno_MC <- rbind(data.frame(i=1,femalesGen),female_geno_MC)
   female_pheno_MC <- rbind(data.frame(i=1,FemaleTrainPop@pheno[,1]-mean(FemaleTrainPop@pheno[,1])),female_pheno_MC)
-  
+
   gsModelF = emBB(as.matrix(female_pheno_MC[,-1]),as.matrix(female_geno_MC[,-1]))
   if(anyNA(gsModelF$hat)){
     gsModelF$hat=rnorm(length(fit$hat))
-  } 
-  gsModelF = as.matrix(gsModelF$hat[female_pheno_MC$i == i],ncol=1)
+  }
+  ### Need to figure out how to use full Genos but only get new pop data. 
+  gsModelAllFemales = gsModelF$hat
+  ###Subset out the num in training pop?
+  gsModelF <- as.matrix(gsModelAllFemales[(nrow(female_pheno_MC)+1):length(gsModelAllFemales)],ncol=1)
 } else{
   
   gsModelM = RRBLUP(MaleTrainPop)
